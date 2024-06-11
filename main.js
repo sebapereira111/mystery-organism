728f987a971a0e6fa79207a03b312cf00da5eded
Sebastian Pereira <157150645+sebapereira111@users.noreply.github.com>
2024-06-11 17:06:30 -0400

    Finalized first part

--- main.js
+++ main.js
@@ -16,12 +16,16 @@ const mockUpStrand = () => {
 /*
    Returns an object that contains the properties specimenNum and dna that correspond to the 
    parameters provided. The first parameter is a number (no two organisms should have the same 
-   number). The second parameter is an array of 15 DNA bases. .mutate() is responsible for randomly 
-   selecting a base in the object’s dna property and changing the current base to a different base. 
-   Then .mutate() will return the object’s dna. .compareDNA() has one parameter, another pAequor 
-   object. Does not return anything, but prints a message that states the percentage of DNA the two 
-   objects have in common.
-
+   number). The second parameter is an array of 15 DNA bases.
+   
+   .mutate() is responsible for randomly selecting a base in the object’s dna property and changing 
+   the current base to a different base. Then .mutate() will return the object’s dna.
+   
+   .compareDNA() has one parameter, another pAequor object. Does not return anything, but prints a 
+   message that states the percentage of DNA the two objects have in common.
+   
+   .willLikelySurvive() returns true if the object’s .dna array contains at least 60% 'C' or 'G' 
+   bases. Otherwise, .willLikelySurvive() returns false.
 */
 const pAequorFactory = (number, arr) => {
   const pAequor = {
@@ -44,20 +48,36 @@ const pAequorFactory = (number, arr) => {
         if (other.dna[i] === this.dna[i]) equal++;
       }
       console.log(`specimen #${this.specimenNum} and specimen #${other.specimenNum} have ${Math.round((equal / 15) * 100)}% DNA in common.`);
+    },
+    willLikelySurvive() {
+      let chance = 0;
+      this.dna.forEach(element => {if (element === 'C' || element === 'G') chance++;});
+      if (chance >= 9) return true;
+      return false;
     }
   }
-  // .willLikelySurvive() returns true if the object’s .dna array contains at least 60% 'C' or 'G' bases. Otherwise, .willLikelySurvive() returns false.
   return pAequor;
 }
 
 
 // With the factory function set up, your team requests that you create 30 instances of pAequor that can survive in their natural environment. Store these instances in an array for your team to study later.
 
-let prueba1 = pAequorFactory(1, mockUpStrand());
 
-let prueba2 = pAequorFactory(2, mockUpStrand());
 
-console.log(prueba1.dna);
-console.log(prueba2.dna);
+// testing the code !!!!!!
 
-prueba1.compareDNA(prueba2);
+console.time('test');
+
+const pAequor30 = [30];
+
+for (let i = 0 ; i < 30 ; i++) {
+  console.log(i);
+  pAequor30[i] = pAequorFactory(i, mockUpStrand());
+  if (!pAequor30[i].willLikelySurvive()) i--;
+}
+
+for (i = 0 ; i < 30 ; i++) {
+  console.log(`Specimen #${i} will survive? > ${pAequor30[i].willLikelySurvive()}`);
+}
+
+console.timeEnd('test');
